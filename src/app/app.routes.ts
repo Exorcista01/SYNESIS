c95d7a058003b776a06749bd6001efab0764bbf3
Jorge Luis <jorgeluisds1853@gmail.com>
2025-09-22 16:50:31 -0300

    conclusão do login,registro e forgoau password

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,8 @@ import { VerticalLayoutComponent } from './core/components/vertical-layout/verti
 import { DashboardComponent } from './features/dashboard/dashboard.component';
 import { CalendaComponent } from './features/apps/calendar/calendar.component';
 import { LoginComponent } from './features/auth/auth/login/login.component';
+import { RegisterComponent } from './features/auth/auth/register/register.component';
+import { ForgotPasswordComponent } from './features/auth/auth/forgot-password/forgot-password.component';
 
 export const routes: Routes = [
     {
@@ -18,6 +20,13 @@ export const routes: Routes = [
     {
         path: 'Login',
         component: LoginComponent,
-
     },
+    {
+        path: "registration",
+        component: RegisterComponent,
+    },
+    {
+        path: "Forgout-Passoword",
+        component: ForgotPasswordComponent,
+    }
 ];
